a1d75d669b637021584b2de22e386a905b16cbea
angelos <angelos_1993@hotmail.de>
2017-11-30 20:55:05 +0100

    Accordion fast fertig. Pane C und D muss noch ergänzt werden. Style wird noch angepasst.

--- Visualizer/VisProject/src/vis/frame/RightFXPanel.java
+++ Visualizer/VisProject/src/vis/frame/RightFXPanel.java
@@ -1,25 +1,27 @@
 package vis.frame;
 
 import java.awt.Dimension;
-import java.awt.Insets;
 import java.awt.Rectangle;
 
-import javax.swing.JCheckBox;
 import javax.swing.JPanel;
 
+import javafx.collections.FXCollections;
 import javafx.embed.swing.JFXPanel;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Accordion;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 import javafx.scene.control.TitledPane;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.GridPane;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
+import javafx.scene.text.TextAlignment;
 
 public class RightFXPanel {
 	
@@ -39,27 +41,112 @@ public class RightFXPanel {
 
 	     //TODO: Accoridion fertigstellen   
         final Label label = new Label("Einstellungen"); 
-        label.setAlignment(Pos.CENTER); 
-        String t1Text = "live-view:\freeze"; 
-        String t2Text = ""; 
-        String t3Text = ""; 
-        String t4Text = ""; 
+        
+
         String t5Text = ""; 
         String t6Text = ""; 
 
         
-        TitledPane t1 = new TitledPane("Common", new CheckBox("live-view")); 
-        TitledPane t2 = new TitledPane("Pane A: 3D", new CheckBox("single dot / vector"));        
-        TitledPane t3 = new TitledPane("Rotate", new Label(t3Text)); 
-        TitledPane t4 = new TitledPane("Pane B: Bar-Chart", new Label(t4Text));
-        TitledPane t5 = new TitledPane("Pane C: Spline", new Label(t5Text));
-        TitledPane t6 = new TitledPane("Pane D: Scatterplot", new Label(t6Text));
+        TitledPane t1 = new TitledPane();
+		GridPane gridt1 = new GridPane();
+		gridt1.setVgap(4);
+		gridt1.setPadding(new Insets(5, 5, 5, 5));
+		
+		final ToggleGroup groupT1 = new ToggleGroup();
+		
+		RadioButton rbLive = new RadioButton("live-view");
+		rbLive.setToggleGroup(groupT1);
+		rbLive.setSelected(true);
+		gridt1.add(rbLive, 0, 0);
+		
+		RadioButton rbFreeze = new RadioButton("freeze");
+		rbFreeze.setToggleGroup(groupT1);
+		gridt1.add(rbFreeze, 1, 0);        
+		
+		t1.setText("Common");
+		t1.setContent(gridt1);
+        
+         
+        TitledPane t2 = new TitledPane();        
+        GridPane gridt2 = new GridPane();
+        gridt2.setVgap(4);
+        gridt2.setPadding(new Insets(5, 5, 5, 5));
+        
+		final ToggleGroup groupT2 = new ToggleGroup();
+		
+		RadioButton rbDotVector = new RadioButton("single dot /vector");
+		rbDotVector.setToggleGroup(groupT2);
+		rbDotVector.setSelected(true);
+		gridt2.add(rbDotVector, 0, 0, 3, 1);
+		
+		RadioButton rbPlotAll = new RadioButton("plot all");
+		rbPlotAll.setToggleGroup(groupT2);
+		gridt2.add(rbPlotAll, 0, 1, 3, 1);
+        
+		RadioButton rbPlotBetween = new RadioButton("plot between");
+		rbPlotBetween.setToggleGroup(groupT2);
+		gridt2.add(rbPlotBetween, 0, 2, 3, 1);
+		
+		TextField txtPlotFrom = new TextField();
+		txtPlotFrom.setMaxWidth(70);
+		gridt2.add(txtPlotFrom, 0, 3);
+		
+		Label lblTo = new Label("to");
+		lblTo.setMinWidth(23);
+		gridt2.add(lblTo, 1, 3);
+		
+		TextField txtPlotTo = new TextField();
+		txtPlotTo.setMaxWidth(70);
+		gridt2.add(txtPlotTo, 2, 3);
+		
+		final Label lblRotate = new Label("Rotate");
+
+		GridPane.setHalignment(lblRotate, HPos.CENTER);
+		gridt2.add(lblRotate, 0, 4, 3, 1);
+		
+		Button rotateLeft = new Button("90° left");
+		rotateLeft.setMinWidth(70);
+		gridt2.add(rotateLeft, 0, 5);
+		
+		Button rotateRight = new Button("90° right");
+		rotateRight.setMinWidth(70);
+		gridt2.add(rotateRight, 2, 5);
+		
+        t2.setText("Pane A: 3D");
+        t2.setContent(gridt2);
+        
+        
+        TitledPane t3 = new TitledPane();
+        GridPane gridt3 = new GridPane();
+        gridt3.setVgap(4);
+        gridt3.setPadding(new Insets(5, 5, 5, 5));
+        
+		final ToggleGroup groupT3 = new ToggleGroup();
+		
+		RadioButton rbShowX = new RadioButton("show x");
+		rbShowX.setToggleGroup(groupT3);
+		rbDotVector.setSelected(true);
+		gridt3.add(rbShowX, 0, 0, 3, 1);
+		
+		RadioButton rbShowY = new RadioButton("show y");
+		rbShowY.setToggleGroup(groupT3);
+		gridt3.add(rbShowY, 0, 1, 3, 1);
+        
+		RadioButton rbShowZ = new RadioButton("show z");
+		rbShowZ.setToggleGroup(groupT3);
+		gridt3.add(rbShowZ, 0, 2, 3, 1);
+        
+        t3.setText("Pane B: Bar-Chart");
+        t3.setContent(gridt3);
+        
+        TitledPane t4 = new TitledPane("Pane C: Spline", new Label(t5Text));
+        TitledPane t5 = new TitledPane("Pane D: Scatterplot", new Label(t6Text));
 
         
         Accordion accordion = new Accordion(); 
-        accordion.getPanes().addAll(t1, t2, t3, t4); 
-        accordion.setExpandedPane(t3); 
-        
+        accordion.getPanes().addAll(t1, t2, t3, t4, t5); 
+        accordion.setExpandedPane(t1); 
+        accordion.setPrefSize(rightSidebarWidth-20, 200);
         
         GridPane grid = new GridPane(); 
         grid.setAlignment(Pos.TOP_CENTER); 
