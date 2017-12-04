83babfe9b1734b0a7eb03117ee1fe973344184d4
eliquidbw <ooner@live.de>
2017-12-05 00:20:37 +0100

    adding distance-indication plus tactile and acustic feedback

--- Probe/tsh/src/com/tsh/controller/TSHMain.java
+++ Probe/tsh/src/com/tsh/controller/TSHMain.java
@@ -1,11 +1,11 @@
 package com.tsh.controller;
 
-
 import java.io.IOException;
 
 import com.pi4j.io.i2c.I2CFactory.UnsupportedBusNumberException;
 import com.tsh.common.MsgTransformer;
 import com.tsh.gpio.ADXL345;
+import com.tsh.gpio.Buzzer;
 import com.tsh.gpio.Ultrasonic;
 import com.tsh.view.GUI;
 
@@ -15,29 +15,43 @@ public class TSHMain {
 	private static MsgTransformer msg2;
 	private static ADXL345 gyro;
 	private static int[] vector = new int[3];
+	private static Buzzer buzz;
+
 	/**
 	 * @param args
-	 * @throws InterruptedException 
-	 * @throws UnsupportedBusNumberException 
-	 * @throws IOException 
+	 * @throws InterruptedException
+	 * @throws UnsupportedBusNumberException
+	 * @throws IOException
 	 */
 	public static void main(String[] args) throws InterruptedException, IOException, UnsupportedBusNumberException {
 		us = new Ultrasonic();
 		gyro = new ADXL345();
 		msg2 = new MsgTransformer();
-		
+		buzz = new Buzzer();
+
 		GUI gui = new GUI("The Seeing Hand");
-		while(true){
+		while (true) {
 
 			msg = us.getDistance();
 			msg2.setVector(gyro.getVector());
-			if(msg.getValue()>0){
+			if (msg.getValue() > 0) {
 				gui.setValueDisplay(msg);
 				gui.setDebugMsg(msg2.getVector());
+				if (msg.getValue() < 15d) {
+					if (msg.getValue() < 7d) {
+						if (msg.getValue() < 6d) {
+							buzz.getFeedback(20);
+						} else {
+							buzz.getFeedback(50);
+						}
+					} else {
+						buzz.getFeedback(100);
+
+					}
+				}
 				System.out.println(msg);
 			}
-			
-			
+
 		}
 
 	}
