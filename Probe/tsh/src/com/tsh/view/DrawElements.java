7e85a0161ac4c4f06dd4827b40ae75a981066761
eliquidbw <ooner@live.de>
2017-12-13 01:08:32 +0100

    Parallel-Vis

--- Probe/tsh/src/com/tsh/view/DrawElements.java
+++ Probe/tsh/src/com/tsh/view/DrawElements.java
@@ -1,12 +1,35 @@
 package com.tsh.view;
 
-public class DrawElements {
+import java.awt.*;
+
+import javax.swing.*;
+
+public class DrawElements extends JFrame {
+
+	Container c;
+	GraphicElements z;
+
+	public DrawElements() {
+		c = getContentPane();
+		z = new GraphicElements();
+		c.add(z);
+
+	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		DrawElements d = new DrawElements();
+		d.setTitle("test");
+		d.setSize(800, 800);
+		d.setVisible(true);
+		d.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		for(int i =10; i<500;i++) {
+			d.z.drawLine(i,i+200);
+			i*=2;
+		}
+		
 
 	}
 
