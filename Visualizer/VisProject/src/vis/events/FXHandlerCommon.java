a5f40456884a1b822f8440f76d169026d693deea
Firewolf <christian.steinmann@student.reutlingen-university.de>
2017-12-19 20:02:07 +0100

    merge

---
+++ Visualizer/VisProject/src/vis/events/FXHandlerCommon.java
@@ -0,0 +1,15 @@
+package vis.events;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+public class FXHandlerCommon implements EventHandler<ActionEvent> {
+
+	
+	@Override
+	public void handle(ActionEvent arg0) {
+		System.out.println("Test");
+		
+	}
+
+}
---
+++ Visualizer/VisProject/src/vis/events/FXHandlerPaneC.java
@@ -0,0 +1,14 @@
+package vis.events;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+public class FXHandlerPaneC implements EventHandler<ActionEvent> {
+
+	@Override
+	public void handle(ActionEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
