80c9fd56fa40507cf305516a3c341a8fd26a62db
eliquidbw <ooner@live.de>
2017-12-19 15:48:13 +0100

    Updated Client/Server-Classes

--- Probe/tsh/src/com/tsh/net/TSHServer.java
+++ Probe/tsh/src/com/tsh/net/TSHServer.java
@@ -1,9 +1,71 @@
 package com.tsh.net;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class TSHServer {
+
+	private static SimpleDateFormat time = new SimpleDateFormat("'Current time: 'HH:mm:ss");
+	static SimpleDateFormat date = new SimpleDateFormat("'Current date: 'yyyy-MM-dd");
+	private Socket s;
+	private BufferedReader incomingMsg;
+	private PrintWriter outgoingMsg;
+
+	private static String hostname = "www.liquidsolution.de";
+	private static int port = 2222;
+
+	private static ServerSocket server;
+	private static Socket socket;
+
+	public TSHServer() {
+		System.out.println("Starting Server");
+		try {
+			server = new ServerSocket(port);
+			socket = server.accept();
+			// s = new Socket();
+			incomingMsg = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			outgoingMsg = new PrintWriter(socket.getOutputStream(), true);
+		} catch (ArrayIndexOutOfBoundsException ae) {
+			System.out.println("Please use: java TSHServer <port>");
+			ae.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("IO-Error");
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		TSHServer tsh = new TSHServer();
+			tsh.transact();
+	}
+
+	public void transact() {
+		//System.out.println("Starting Protocol");
+		try {
+			outgoingMsg.println("Enter CMD: {DATE, TIME}");
+			String expectDate = incomingMsg.readLine();
+			Date currentDate = new Date();
+			if (expectDate.equalsIgnoreCase("date")) {
+				outgoingMsg.println(date.format(currentDate));
+			} else if (expectDate.equalsIgnoreCase("time")) {
+				outgoingMsg.println(time.format(currentDate));
+			} else {
+				outgoingMsg.println(expectDate + " wrong command!");
+			}
+		} catch (IOException e) {
+			System.out.println("IO-Error");
+			e.printStackTrace();
+		}
+		//System.out.println("Finished Protocol");
+		transact();
+	}
 
 	public InetAddress getHost(String hostname) {
 		System.out.println("Host-Check started...");
@@ -19,30 +81,4 @@ public class TSHServer {
 		}
 		return ip;
 	}
-
-	public static void setServer(int port) {
-		ServerSocket server;
-		try {
-			server = new ServerSocket(port);
-			Socket socket = server.accept();
-			new TSHClock(socket).transact();
-		} catch (ArrayIndexOutOfBoundsException ae) {
-			System.out.println("Please use: java TSHServer <port>");
-			ae.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-
-		}
-
-	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		TSHServer tsh = new TSHServer();
-		tsh.getHost("www.liquidsolution.de");
-
-		setServer(2222);
-
-	}
-
 }
