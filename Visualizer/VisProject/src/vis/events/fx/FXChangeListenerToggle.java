ba12bcca635019eede7033f55e67d3d8aeffc1d0
F1r3w0lf <F1r3w0lf@users.noreply.github.com>
2018-01-05 10:38:28 +0100

    fix

---
+++ Visualizer/VisProject/src/vis/events/fx/FXChangeListenerToggle.java
@@ -0,0 +1,15 @@
+package vis.events.fx;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.control.Toggle;
+
+public class FXChangeListenerToggle implements ChangeListener<Toggle> {
+
+	@Override
+	public void changed(ObservableValue<? extends Toggle> arg0, Toggle arg1, Toggle arg2) {
+		
+		
+	}
+
+}
--- Visualizer/VisProject/src/vis/events/fx/FXHandlerPaneB.java
+++ Visualizer/VisProject/src/vis/events/fx/FXHandlerPaneB.java
@@ -8,6 +8,19 @@ public class FXHandlerPaneB implements EventHandler<ActionEvent> {
 	@Override
 	public void handle(ActionEvent event) {
 		System.out.println("Action 2: " + event.getSource());
+		
+		if(event.getSource().toString().contains("2b2948e2"));
+			System.out.println("Es ist: show x");
+		if (event.getSource().toString().contains("6ddf90b0")) 
+			System.out.println("Es ist: show y");
+		if (event.getSource().toString().contains("6ddf90b0")) 
+			System.out.println("Es ist: show y");
+	
+	
+		
+		
+		
+		System.out.println(event.getTarget().toString());
 	}
 
 }
