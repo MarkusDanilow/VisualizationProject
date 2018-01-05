a42626151cf368e77bd615891c72833c692cae0a
F1r3w0lf <F1r3w0lf@users.noreply.github.com>
2018-01-05 12:25:12 +0100

    FXHandler -> ChangeListener

--- Visualizer/VisProject/src/vis/events/fx/FXHandlerPaneC.java
+++ Visualizer/VisProject/src/vis/events/fx/FXHandlerPaneC.java
@@ -1,18 +1,19 @@
 package vis.events.fx;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.beans.value.ObservableValue;
 import vis.frame.MainWindow;
 
-public class FXHandlerPaneC extends FXEventHandler {
+public class FXHandlerPaneC extends FXChangeListenerObject {
 
 	public FXHandlerPaneC(MainWindow wnd) {
 		super(wnd);
 	}
 
 	@Override
-	public void handle(ActionEvent event) {
-		System.out.println("Action 2: " + event.getSource());
+	public void changed(ObservableValue<? extends Object> observable, Object oldValue, Object newValue) {
+		System.out.println(observable);
+		System.out.println(oldValue);
+		System.out.println(newValue);
 	}
 
 }
--- Visualizer/VisProject/src/vis/events/fx/FXHandlerPaneD.java
+++ Visualizer/VisProject/src/vis/events/fx/FXHandlerPaneD.java
@@ -1,18 +1,19 @@
 package vis.events.fx;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.beans.value.ObservableValue;
 import vis.frame.MainWindow;
 
-public class FXHandlerPaneD extends FXEventHandler {
+public class FXHandlerPaneD extends FXChangeListenerObject {
 
 	public FXHandlerPaneD(MainWindow wnd) {
 		super(wnd);
 	}
 
 	@Override
-	public void handle(ActionEvent event) {
-		System.out.println("Action 2: " + event.getSource());
+	public void changed(ObservableValue<? extends Object> observable, Object oldValue, Object newValue) {
+		System.out.println(observable);
+		System.out.println(oldValue);
+		System.out.println(newValue);
 	}
 
 }
